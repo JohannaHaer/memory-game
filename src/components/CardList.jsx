546c0a13d82776b69a1a7238f52ed4a6ee863154
Pavlo Nikulin <pavlo.nikulin@obi.de>
2025-08-28 22:41:30 +0200

    start implementation of the matching logic

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,34 +1,66 @@
-import React, { useContext } from 'react'
-import { mainContext } from '../context/mainProvider'
-import CardItem from './CardItem';
+import React, { useContext } from "react";
+import { mainContext } from "../context/mainProvider";
+import CardItem from "./CardItem";
 
 const CardList = () => {
-    const {cards} = useContext(mainContext)
-    console.log(cards);
+  const { data } = useContext(mainContext);
+  const cards = data.map((d) => ({
+    ...d,
+    opened: false,
+  }));
 
-    const shuffle = () => {
-        window.location.reload();
+  const openedNotMatched = null;
+
+  const handleFlip = (cardId) => {
+    // Wenn Karte mit cardID schon gematched ist, dann beenden.
+    if (cardId.matched == true) {
+      return;
     }
 
-    for (let i = cards.length - 1; i > 0; i--) {
-        let j = Math.floor(Math.random() * (i + 1));
-        let temp = cards[i];
-        cards[i] = cards[j];
-        cards[j] = temp;
-    }
+    // Karte mit cardID opened = true
+    cards.map((c) => {
+      if (c.id == cardId) {
+        ({
+          ...c,
+          opened: true,
+        });
+      } else c;
+    });
+
+    // Wenn openedNotMatched leer ist,
+    //      cardID number einfügen
+    // Ansonsten
+    //      Wenn openedNotMatched != null und cardID number = zweiter cardID number ist
+    //          openedNotMatched = null
+    //          matched = true für beide
+    //      Ansonsten
+    //          openedNotMatched = null
+    //          opened = false für beide
+  };
+
+  const shuffle = () => {
+    window.location.reload();
+  };
+
+  for (let i = cards.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = cards[i];
+    cards[i] = cards[j];
+    cards[j] = temp;
+  }
 
   return (
     <>
-        <section className='playground'>
+      <section className="playground">
         {cards.map((card) => {
-            return(
-                <CardItem key={card.id} link={card.link} number={card.number}/>
-            )
+          return (
+            <CardItem key={card.id} link={card.link} number={card.number} />
+          );
         })}
-        </section>
-        <button onClick={shuffle}>New Game</button>
+      </section>
+      <button onClick={shuffle}>New Game</button>
     </>
-  )
-}
+  );
+};
 
-export default CardList
+export default CardList;
